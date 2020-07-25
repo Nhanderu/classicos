66ae044702dd6780da90e82e488e94d8b27bfe65
Rafael Escobar <rescobar@redventures.com>
2020-07-24 23:19:05 -0300

    Call the API

--- src/fetch.js
+++ src/fetch.js
@@ -1,16 +1,18 @@
-const query = `query Seila {
-    team(abbr: "sccp") {
-        name
-        full_name
-        trophies(champ_slug: "national-league-1-div") {
-            year
-            champ {
-                slug
-                name
+const query = `
+    query Seila {
+        team(abbr: "sccp") {
+            name
+            full_name
+            trophies(champ_slug: "national-league-1-div") {
+                year
+                champ {
+                    slug
+                    name
+                }
             }
         }
     }
-}`;
+`;
 
 axios.post('https://gorduchinha.herokuapp.com/api/v1/graphql', { query })
     .then(res => console.log('ok', res))
